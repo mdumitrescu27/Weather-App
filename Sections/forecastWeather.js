65e1bb9d3e9f31aa7586dc7a72baf8ddbd65ef5f
mdumitrescu27 <114684484+mdumitrescu27@users.noreply.github.com>
2022-11-22 19:23:49 +0200

    advance forecast display

--- Sections/forecastWeather.js
+++ Sections/forecastWeather.js
@@ -8,27 +8,42 @@ function displayForecastWeather(city) {
 
       weatherForecastContainerElement.innerHTML = "";
 
+      const daysMap = {};
       list.forEach((element) => {
-        const { dt, main, weather } = element;
+        const { dt } = element;
         const day = getDayOfWeek(dt);
-        const hour = getHour(dt);
-        const temperature = Math.round(main.temp);
-        const realFeel = Math.round(main.feels_like);
-        const weatherDescription = weather[0].description;
-        const weatherIcon = getWeatherIcon(weather[0].icon);
-
-        weatherForecastContainerElement.innerHTML += `
-        <div class="weather-forecast-box w-100 d-flex justify-content-between align-items-center">
-            <div>
-                <div><strong>${day}</strong></div>
-                <div>${hour}</div>
-            </div>
-            <div><img src=${weatherIcon} alt="" /></div>
-            <div><strong>${temperature} °C</strong></div>
-            <div>${weatherDescription}</div>
-            <div>Real feel: ${realFeel} °C</div>
-        </div>
-        `;
+        if (daysMap[day]) {
+          daysMap[day].push(element);
+        } else {
+          daysMap[day] = [element];
+        }
       });
+
+      for (key in daysMap) {
+        weatherForecastContainerElement.innerHTML += `<h3 class="text-primary">${key}</h3>`;
+
+        const days = daysMap[key];
+
+        days.forEach((element) => {
+          const { dt, main, weather } = element;
+          const hour = getHour(dt);
+          const temperature = Math.round(main.temp);
+          const realFeel = Math.round(main.feels_like);
+          const weatherDescription = weather[0].description;
+          const weatherIcon = getWeatherIcon(weather[0].icon);
+
+          weatherForecastContainerElement.innerHTML += `
+          <div class="weather-forecast-box w-100 d-flex justify-content-between align-items-center border rounded p-3 m-3">
+              <div>
+                  <div>${hour}</div>
+              </div>
+              <div><img src=${weatherIcon} alt="" /></div>
+              <div><strong>${temperature} °C</strong></div>
+              <div>${weatherDescription}</div>
+              <div>Real feel: ${realFeel} °C</div>
+          </div>
+          `;
+        });
+      }
     });
 }
